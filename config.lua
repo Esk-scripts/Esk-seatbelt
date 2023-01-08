2e62b30b06988585ecbb1bf38517f56ebfdabc27
Esk Scripts <122018696+Esk-scripts@users.noreply.github.com>
2023-01-08 15:57:07 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.Sounds = true
+Config.LoopSound = false
+Config.Volume = 0.8
+-- Min: 0.0 Max: 1.0
+
+Config.Notification = true
+Config.Strings = {seatbelt_on = 'Te has abrochado el cinturon', seatbelt_off = 'Te has desabrochado el cinturon'}
+
+Config.Blinker = true
+
+Config.Control = 58
+
+Config.Speed = 60.0
+-- Decimales si o si
+
+Config.AlarmOnlySpeed = true
+Config.AlarmSpeed = 20
+
